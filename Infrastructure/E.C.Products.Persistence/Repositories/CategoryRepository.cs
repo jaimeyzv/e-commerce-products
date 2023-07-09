1087573829643b8b04cb0376aa8e621ba4be0e19
Jaime Zamora <jyzamorav@gmail.com>
2023-07-09 01:00:31 -0500

    custom dbcontext references

--- Infrastructure/E.C.Products.Persistence/Repositories/CategoryRepository.cs
+++ Infrastructure/E.C.Products.Persistence/Repositories/CategoryRepository.cs
@@ -1,10 +1,18 @@
 ﻿using E.C.Products.Application.Interfaces.Persistence;
 using E.C.Products.Domain.Entities;
+using E.C.Products.Persistence.Shared;
 
 namespace E.C.Products.Persistence.Repositories
 {
     public class CategoryRepository : ICategoryRepository
     {
+        private ECProductsDbContext _dbContext;
+
+        public CategoryRepository(ECProductsDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
         public Task AddAsync(Category entity)
         {
             throw new NotImplementedException();
